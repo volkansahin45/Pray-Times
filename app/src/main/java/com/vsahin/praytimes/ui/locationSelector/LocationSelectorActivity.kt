45a076697a548f24236a897fabe7309c13a99580
Volkan Sahin <volkan.sahin@accedo.tv>
2021-12-09 14:27:18 +0100

    Migrate LocationSelectorActivity to Jetpack Compose

--- app/src/main/java/com/vsahin/praytimes/ui/locationSelector/LocationSelectorActivity.kt
+++ app/src/main/java/com/vsahin/praytimes/ui/locationSelector/LocationSelectorActivity.kt
@@ -1,81 +1,28 @@
 package com.vsahin.praytimes.ui.locationSelector
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View.GONE
-import android.view.View.VISIBLE
+import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.wear.widget.WearableLinearLayoutManager
-import com.vsahin.praytimes.databinding.ActivityLocationSelectorBinding
-import com.vsahin.praytimes.ui.home.HomeActivity
+import androidx.wear.compose.material.ExperimentalWearMaterialApi
+import com.vsahin.praytimes.ui.theme.PrayTimesTheme
 import dagger.hilt.android.AndroidEntryPoint
-import com.vsahin.praytimes.ui.tile.PrayTileService
-import androidx.wear.tiles.TileService
 
 @AndroidEntryPoint
 class LocationSelectorActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityLocationSelectorBinding
     private val viewModel: LocationSelectorViewModel by viewModels()
 
+    @ExperimentalWearMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityLocationSelectorBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.locationRecyclerView.apply {
-            isEdgeItemsCenteringEnabled = true
-            layoutManager =
-                WearableLinearLayoutManager(
-                    this@LocationSelectorActivity,
-                    CustomScrollingLayoutCallback()
+        setContent {
+            PrayTimesTheme {
+                LocationSelectorScreen(
+                    viewModel = viewModel,
+                    onFinish = { finish() }
                 )
-        }
-
-        viewModel.state.observe(this, { state ->
-            val locationType = state.locationType
-            val locations = state.locations
-            binding.loadingProgressBar.visibility = if (state.isLoading) VISIBLE else GONE
-            binding.locationRecyclerView.adapter =
-                LocationSelectorAdapter(locationType.toString(), locations, object :
-                    LocationSelectorAdapter.LocationClickListener {
-                    override fun onLocationClick(index: Int) {
-                        viewModel.setLocationId(locations[index].id)
-                        when (locationType) {
-                            LocationType.COUNTRY -> viewModel.setLocationType(LocationType.CITY)
-                            LocationType.CITY -> viewModel.setLocationType(LocationType.DISTRICT)
-                            LocationType.DISTRICT -> {
-                                viewModel.setLocationName(locations[index].name)
-                                viewModel.saveLocationName()
-                                viewModel.saveLocationIds()
-                                refreshTile()
-                                val intent =
-                                    Intent(this@LocationSelectorActivity, HomeActivity::class.java)
-                                startActivity(intent)
-                                finish()
-                            }
-                        }
-                    }
-                })
-        })
-    }
-
-    private fun refreshTile() {
-        TileService.getUpdater(this@LocationSelectorActivity)
-            .requestUpdate(PrayTileService::class.java)
-    }
-
-    override fun onBackPressed() {
-        when (viewModel.state.value?.locationType) {
-            LocationType.CITY -> {
-                viewModel.setLocationType(LocationType.COUNTRY)
             }
-            LocationType.DISTRICT -> {
-                viewModel.setLocationType(LocationType.CITY)
-            }
-            else -> super.onBackPressed()
         }
     }
 }
