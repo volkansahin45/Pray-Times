edee0395afb50a67cede3963e796ceeef09333a3
Volkan Şahin <volkansahindev@gmail.com>
2021-12-13 16:52:09 +0100

    WIP - Migrating to Compose Navigation

---
+++ app/src/main/java/com/vsahin/praytimes/ui/NavigationGraph.kt
@@ -0,0 +1,41 @@
+package com.vsahin.praytimes.ui
+
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.wear.compose.material.ExperimentalWearMaterialApi
+import androidx.wear.compose.navigation.SwipeDismissableNavHost
+import androidx.wear.compose.navigation.composable
+import androidx.wear.compose.navigation.rememberSwipeDismissableNavController
+import com.vsahin.praytimes.ui.about.AboutScreen
+import com.vsahin.praytimes.ui.home.HomeScreen
+import com.vsahin.praytimes.ui.locationSelector.LocationSelectorScreen
+
+const val HOME = "home"
+const val LOCATION_SELECTOR = "locationSelector"
+const val ABOUT = "about"
+
+@ExperimentalWearMaterialApi
+@Composable
+fun Navigation() {
+    val navController = rememberSwipeDismissableNavController()
+    SwipeDismissableNavHost(
+        navController = navController,
+        startDestination = HOME,
+    ) {
+        composable(HOME) {
+            HomeScreen(
+                navController = navController,
+                viewModel = hiltViewModel(),
+            )
+        }
+        composable(LOCATION_SELECTOR) {
+            LocationSelectorScreen(
+                navController = navController,
+                viewModel = hiltViewModel()
+            )
+        }
+        composable(ABOUT) {
+            AboutScreen()
+        }
+    }
+}
